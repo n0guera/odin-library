7d58e09916100b6e8ec566538fba4d6d953ff470
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-04-18 11:47:18 -0300

    Add event listener for submit button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 let myLibrary = [];
 
+const newBookForm = document.querySelector('#new-book-form');
+const bookForm = document.querySelector('#book-form');
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
+
+  const book = new Book(title, author, pages, read);
+
+  myLibrary.push(book);
+});
