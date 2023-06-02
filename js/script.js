def3f30dd0ecc0e98144ebe4e658282f43f1e178
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-06-02 18:11:23 -0300

    Change book constructor to class

--- js/script.js
+++ js/script.js
@@ -64,9 +64,11 @@ function addBookToLibrary() {
   showBooks();
 }
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
